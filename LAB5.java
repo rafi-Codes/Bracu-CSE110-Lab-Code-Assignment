898d4b8454438ff0b66caeecdf50a6b3c7ccb478
Mohammad Rafiul Hasan <rafihasan27rafi@gmail.com>
2025-12-09 09:09:41 +0600

    Update LAB5.java

--- LAB5.java
+++ LAB5.java
@@ -59,7 +59,7 @@ public class task02 {
 
 // Task 03
 import java.util.Scanner;
-public class codeforces {
+public class task03 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner (System.in);
@@ -184,7 +184,7 @@ public class taask06 {
 
 // Task 07
 import java.util.Scanner;
-public class codeforces {
+public class task07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         int n = sc.nextInt();
